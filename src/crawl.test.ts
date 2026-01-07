560b61c2b313e58c27d33647247c685aaa403a35
Bianca <103384402+TokiLoshi@users.noreply.github.com>
2026-01-07 09:26:05 +0200

    adding normalized url

---
+++ src/crawl.test.ts
@@ -0,0 +1,17 @@
+// take a single url string and return normalized version as string
+import { expect, test } from "vitest";
+import normalizeURL from "./crawl";
+
+test("normalize URL protocol", () => {
+	const input = "https://blog.boot.dev/path";
+	const actual = normalizeURL(input);
+	const expected = "blog.boot.dev/path";
+	expect(actual).toEqual(expected);
+});
+
+test("normalize URL slash", () => {
+	const input = "https://blog.boot.dev/path/";
+	const actual = normalizeURL(input);
+	const expected = "blog.boot.dev/path";
+	expect(actual).toEqual(expected);
+});
---
+++ src/crawl.ts
@@ -0,0 +1,8 @@
+export default function normalizeURL(url: string) {
+	const urlObj = new URL(url);
+	let fullPath = `${urlObj.host}${urlObj.pathname}`;
+	if (fullPath.slice(-1) === "/") {
+		fullPath = fullPath.slice(0, -1);
+	}
+	return fullPath;
+}
