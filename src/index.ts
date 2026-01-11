328042f0f1f20b3b9655775e3242a65f171e84f7
Bianca <103384402+TokiLoshi@users.noreply.github.com>
2026-01-11 10:31:46 +0200

    adding crawling to main

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello, World!");
+function main() {
+	console.log("Hello, World! Let's crawl some websites.");
+	const testURL = "https://wagslane.dev";
+	console.log("Base URL: ", testURL);
+	const args = process.argv.slice(2);
+	console.log("Args in main: ", args);
+	if (args.length < 1) {
+		console.error("Args must be a length of 1, not enough args");
+		process.exit(1);
+	} else if (args.length > 1) {
+		console.error("Args must have a length of 1, too many args");
+		process.exit(1);
+	}
+
+	if (args.length === 1) {
+		const baseURL = args[0];
+		console.log("starting to crawl: ", baseURL);
+	}
+}
+
+main();
